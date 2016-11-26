747ab263002fddfafdfe8a3c4941f2b78184d520
Guillaume Lavoie <guillaume.laboie-harvey@polymtl.ca>
2016-11-26 18:10:14 -0500

    works?

--- TP5/client/app/app.component.ts
+++ TP5/client/app/app.component.ts
@@ -3,16 +3,11 @@ import {Component} from "@angular/core";
 @Component({
     selector: `mon-app`,
     template: `
-        <h1>{{title}}</h1>
-        <nav>
-            <a routerLink="/dashboard">Dashboard</a>
-            <a routerLinkActive="active" routerLink="/heroes">Heroes</a>
-        </nav>
         <router-outlet></router-outlet>
     `
 })
 
 
 export class AppComponent {
-    title = "Tour of heroes";
+    
 }
