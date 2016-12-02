a2155b9419975fc29f3c2b87a74d10f0934f768c
Guillaume Lavoie <guillaume.laboie-harvey@polymtl.ca>
2016-12-02 15:26:28 -0500

    detailed stats

--- TP5/client/app/components/stats-details-popup.component.ts
+++ TP5/client/app/components/stats-details-popup.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import {User} from "../objects/user";
+import {UserService} from "../services/user.service";
+import {QuestionService} from "../services/question.service";
 
 @Component({
   selector: 'my-stats-details',
@@ -6,16 +9,40 @@ import { Component, Input } from '@angular/core';
 })
 
 export class StatsDetailsPopupComponent {
-	  
-  constructor(
-		//private statsService: StatsService,
-	) {
-      this.hide();
-      
-  }
-  
+
+  user: User;
+  static uIDgenerator: number = 0;
+  uID: number;
+
+
+  // strings
+  testScore: string;
+  examScore: string;
+
+  //Show and hide stats details box
   private isHidden = true;
   private visibleAnimate = false;
+
+  
+  
+  constructor(
+	private userService: UserService,
+    private questionService: QuestionService
+    ) {
+      this.hide();
+      this.uID = StatsDetailsPopupComponent.uIDgenerator++;
+        this.user = new User();
+        questionService.refreshForStats.subscribe( (newUser) => {
+            this.user = newUser;
+            console.log("Updated user by refreshForStats in Stats by " + this.uID);
+            this.populateStatistics();
+        });
+        userService.newUser.subscribe( (newUser) => {
+            this.user = newUser;
+            console.log("Updated user by newUser in Stats");
+            this.populateStatistics();
+        });
+    }
 
   public show(): void {
   console.log("showing");
@@ -30,4 +57,30 @@ export class StatsDetailsPopupComponent {
   public getVisibility() {
       return this.isHidden ? "modalInVisible" : "modalVisible";
   }
+  
+  
+  //Actual stats
+
+    private initialise() {
+        this.userService.getUser().then(data => {
+            this.user = data;
+            console.log("Updated user by init in Stats");
+            this.populateStatistics();
+        });
+    }
+
+    populateStatistics() : void {
+        this.testScore = (this.user.test.score+this.user.test.currenttest.score || 0) + " / " + (this.user.test.total+this.user.test.currenttest.total || 0);
+        this.examScore = this.calculatePercent(this.user.examen.score, this.user.examen.total) + "%";
+    }
+
+    ngOnInit() : void {
+        this.initialise();
+    }
+
+    calculatePercent(score: number, total: number) : number {
+        return (Math.round(score / total * 100) || 0);
+    }
+
+  
 }
